6d8e6fc47a65d7a52ef2c689ee4810cc10737df0
admin@ammar <admin@ammar.awash>
2021-08-04 02:43:27 +0300

    added

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    localStorage
+    /// gfhjsd
+});
+const btns = {
+    "up" : document.querySelector('.signup'),
+    "in": document.querySelector('.signin'),
+    "out" : document.querySelector('.signout')
+}
+btns.up.addEventListener("click", () =>{
+
+});
